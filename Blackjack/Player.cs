7b50d9a2c8b36cb4a2fe50817b6733940b6c0fc0
Elmer <elmermb13@gmail.com>
2018-05-13 02:33:41 -0600

    Add Player class

---
+++ Blackjack/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack {
+
+    public class Player : GenericPlayer {
+
+        public Player(string name) : base(name) {
+        }
+
+        public bool RequestCard() {
+            var answer = "";
+            Console.WriteLine("{0} Would you like one more card? (Y/N): ", m_name);
+            answer = Console.ReadLine();
+
+            return (answer == "y" || answer == "Y"); 
+        }
+
+        public void Win() {
+            Console.WriteLine("{0} wins", m_name);
+        }
+
+        public void Lose() {
+            Console.WriteLine("{0} lose", m_name);
+        }
+
+        public void Push(){
+            Console.WriteLine("{0} push", m_name);
+        }
+    }
+}
